91a7f961255d332c845f2c246ac14c4429fa78c1
ASHISH KUMAR <47477440+asfafasa@users.noreply.github.com>
2020-08-25 14:04:18 +0530

    uploading icice-4

--- src/Navbar/NavBar.js
+++ src/Navbar/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   const hoverCommittee = () => {
@@ -17,14 +18,16 @@ export default function NavBar() {
     <div>
       <div className='bg-blue-500 text-gray-100'>
         <div className='flex xl:flex-row flex-col justify-end text-base'>
-          <div className='p-4 hover:bg-blue-700'>Home</div>
+          <Link to='/' className='p-4 hover:bg-blue-700'>
+            Home
+          </Link>
           <div className='p-4 hover:bg-blue-700'>About KIIT</div>
           <div className='p-4 hover:bg-blue-700'>Call for Paper</div>
           <div className='p-4 hover:bg-blue-700'>Registration</div>
           <div className='p-4 hover:bg-blue-700'>Important Dates</div>
           <div className='dropdown inline rounded hover:bg-blue-500 hover:text-gray-100 z-50'>
             <a
-              href='#'
+              href=''
               className='relative block mr-3 bg-transparent hover:bg-blue-700 p-4 border border-blue-500 hover:border-transparent rounded'>
               Committees ▾
             </a>
