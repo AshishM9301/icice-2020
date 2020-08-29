5c9cb474dae71a988d3d1fdabb0b2016a3d0e947
ASHISH KUMAR <47477440+asfafasa@users.noreply.github.com>
2020-08-29 21:33:56 +0530

    Changing Layouts-1

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -9,22 +9,22 @@ function Header(props) {
     <div>
       <ToastContainer />
       <div className='flex xl:flex-row flex-col justify-between py-8 px-4'>
-        <div className='flex mx-auto justify-between'>
+        <div className='flex flex-row mx-auto justify-between'>
           <div className='xl:w-40 mx-auto w-20 self-center'>
             <img src={logo} className='max-w-full mx-auto' alt='icice logo' />
           </div>
-          <div className='px-4 xl:block hidden'>
+          <div className='px-4 '>
             <div className='xl:text-2xl text-xl text-gray-800 font-spartan font-bold text-center'>
               <div className='text-3xl p-2'>ICICE-2020</div>
-              <div className='text-3xl p-2 pb-4'>
+              <div className='text-3xl xl:block hidden p-2 pb-4'>
                 International Conference on Industry 4.0 & Circular Economy
               </div>
               <hr />
-              <div className='pb-2 pt-4'>
+              <div className=' xl:block hidden pb-2 pt-4'>
                 Organised by School of Mechanical Engineering, Kalinga Institute
                 of Industrial Technology, Bhubaneswar, Odisha
               </div>
-              <div>18th-20th December 2020</div>
+              <div className='xl:block hidden'>18th-20th December 2020</div>
             </div>
           </div>
           <div className='xl:w-40 mx-auto w-20 self-center'>
