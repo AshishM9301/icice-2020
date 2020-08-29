ec4fc4c6d9a9e73db5aa8823aad4e07ab1067f81
ASHISH KUMAR <47477440+asfafasa@users.noreply.github.com>
2020-08-29 20:18:44 +0530

    Changing Layouts

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,20 +5,6 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function Header(props) {
-  {
-    /* if (props.sendMessage) {
-    toast(props.sendMessage, {
-      position: 'top-right',
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-  }*/
-  }
-
   return (
     <div>
       <ToastContainer />
