d7fd9ff6d79de78406153138903a6c1f074ae9d7
ASHISH KUMAR <47477440+asfafasa@users.noreply.github.com>
2020-08-30 14:06:49 +0530

    Updating Page-1

--- src/Components/PaperSubmission/PaperSubmission.js
+++ src/Components/PaperSubmission/PaperSubmission.js
@@ -8,35 +8,50 @@ function PaperSubmission() {
       <div className='font-spartan bg-blue-800 text-gray-300 text-3xl p-4 text-center rounded'>
         Paper Submission
       </div>
-      <div className='text-xl font-spartan px-6 py-2 text-justify my-12'>
-        <div>
-          Paper should be in the format strictly specified on the conference
-          website. Only Author(s) of the selected papers will get the intimation
-          about whose articles will be accepted for publication. Kindly do not
-          submit the paper multiple times; it may lead to cancellation of an
-          article/paper.
-        </div>
-        <div className='border bg-gray-800 my-6 text-gray-100 font-ubuntu text-3xl text-center p-6'>
-          Note: All papers will be checked with advanced plagiarism detection
-          tools like Turn it in or iThenticate.
-        </div>
-        <div className='w-40 my-16 mx-auto'>
-          <img src={download} alt='download' className='max-w-full' />
-        </div>
-        <div className='text-center'>
-          <div className='my-4'>Click here to download the paper format</div>
-          <div className='my-4'>
-            Submit your Article/Review/Research Paper to Submit
-          </div>
-          <a
-            href='https://easychair.org/conferences/?conf=icice2020'
-            target='_blank'
-            rel='noopener noreferrer'>
-            <div className='w-40 mx-auto shadow border p-2 rounded'>
-              <img src={easy} alt='easy' className='max-w-full' />
+      <div className='text-xl font-spartan px-6 py-2 text-justify my-12 '>
+        <ul className='leading-loose list-decimal list-outside'>
+          <li className=''>
+            Paper should be in the format strictly specified on the conference
+            website. Only Author(s) of the selected papers will get the
+            intimation about whose articles will be accepted for publication.
+            Kindly do not submit the paper multiple times; it may lead to
+            cancellation of an article/paper.
+          </li>
+          <li className='my-5'>
+            Note: All papers will be checked with advanced plagiarism detection
+            tools like Turn it in or iThenticate.
+          </li>
+          <li className='my-4'>
+            <a
+              className='flex text-blue-500 flex-wrap'
+              href='https://www.springer.com/de/authors-editors/book-authors-editors/resources-guidelines/book-manuscript-guidelines/manuscript-preparation/5636#c3324'
+              target='_blank'
+              rel='noopener noreferrer'>
+              <div className='self-center'>
+                Click here to download the paper format
+              </div>
+              <div className='w-40 my-16 mx-auto'>
+                <img src={download} alt='download' className='max-w-full' />
+              </div>
+            </a>
+          </li>
+          <li>
+            <div className='flex flex-wrap'>
+              <div className='my-4'>
+                Submit your Article/Review/Research Paper to Submit
+              </div>
+              <a
+                href='https://easychair.org/conferences/?conf=icice2020'
+                target='_blank'
+                className='mx-4'
+                rel='noopener noreferrer'>
+                <div className='w-40  mx-auto shadow border p-2 rounded'>
+                  <img src={easy} alt='easy' className='max-w-full' />
+                </div>
+              </a>
             </div>
-          </a>
-        </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
